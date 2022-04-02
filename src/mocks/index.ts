07653ae81d187f5fff6afc582ea396a261676176
Yz4230 <yuzupon1480@gmail.com>
2022-04-03 00:02:05 +0900

    add mock data

---
+++ src/mocks/index.ts
@@ -0,0 +1,6 @@
+import accounts from "./accounts.json";
+
+import type { Account } from "../type";
+
+export const mockAccount = accounts[0] as Account;
+export const mockAccounts = accounts as Account[];
